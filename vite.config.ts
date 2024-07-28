34a81d10b443031bcb7f5d8eba8148c8d3b75551
Saúl Ramos <saul.ramos@encora.com>
2024-07-28 16:25:23 -0700

    Setup mdx

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import mdx from "@mdx-js/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    { enforce: "pre", ...mdx({}) },
+    react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

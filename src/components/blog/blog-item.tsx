499a2dfdad8433ed962759c499ac61a8990701dd
Saúl Ramos <saul.ramos@encora.com>
2024-07-28 15:49:40 -0700

    Add placeholder blog-item

--- src/components/blog/blog-item.tsx
+++ src/components/blog/blog-item.tsx
@@ -2,12 +2,13 @@ import { FC, ReactNode } from "react";
 import { Link, LinkProps } from "react-router-dom";
 
 import { cn } from "@/lib/utils";
+import { format } from "date-fns";
 
 export const BlogItem: FC<LinkProps> = ({ className, ...props }) => {
   return (
     <Link
       className={cn(
-        "relative flex flex-col w-full overflow-hidden p-2 pb-4 rounded bg-encora-red/15 md:w-1/3",
+        "group relative flex flex-col w-full min-h-24 overflow-hidden p-2 pb-4 rounded-md bg-encora-red/10 border-2 border-encora-red transition-all hover:scale-105 hover:bg-encora-red/15 focus:scale-95 md:w-1/3",
         className
       )}
       {...props}
@@ -37,9 +38,11 @@ export const BlogItemDate: FC<DateProps> = ({ className, date }) => {
   return (
     <span
       className={cn(
-        "absolute bottom-1 right-0 w-1/2 text-center bg-encora-red text-white text-base",
+        "absolute bottom-1 right-0 w-1/2 text-center bg-encora-red text-white text-base transition-all group-hover:w-full",
         className
       )}
-    ></span>
+    >
+      {format(date, "PP")}
+    </span>
   );
 };
--- src/components/blog/blog-section.tsx
+++ src/components/blog/blog-section.tsx
@@ -1,13 +1,18 @@
 import { Section } from "../section/section";
+import { BlogItem, BlogItemDate, BlogItemTitle } from "./blog-item";
 
 export const BlogSection = () => {
   return (
     <Section>
-      <div className="flex flex-col gap-6 border-l-2 border-encora-red pl-2">
-        <h2 className="text-2xl font-bold text-encora-red">
-          Blog
-        </h2>
+      <div className="w-full flex flex-col gap-6 border-l-2 border-encora-red pl-2">
+        <h2 className="text-2xl font-bold text-encora-red">Blog</h2>
         <p>Here, I&apos;ll be documenting my journey via blog entries.</p>
+        <div className="w-full flex items-stretch gap-2 px-2">
+          <BlogItem to="#">
+            <BlogItemTitle>Spark Week 3</BlogItemTitle>
+            <BlogItemDate date={new Date(2024, 6, 28)} />
+          </BlogItem>
+        </div>
       </div>
     </Section>
   );

3043eb63420e18f7b2099e9818ac9b9ebc179b79
Saúl Ramos <saul.ramos@encora.com>
2024-07-28 20:46:32 -0700

    Add deployment workflow, add base URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import mdx from "@mdx-js/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     { enforce: "pre", ...mdx({}) },
     react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ }),
